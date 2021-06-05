323b171fbb88704fc0c99a634ec80d0104c3e56e
Francisco Lucas <fcolucaslima14@gmail.com>
2021-06-04 23:00:43 -0300

    Alterando broker

--- src/service/broker.js
+++ src/service/broker.js
@@ -18,23 +18,3 @@ export const producer = data => {
     }
   });
 };
-
-export const consumer = () => {
-  const client = Stomp.client('ws://localhost:61614/stomp');
-  const headers = { login: 'admin', password: 'admin' };
-
-  const destination = '/topic/ActiveMQ.Advisory.Topic';
-
-  client.connect(headers, error => {
-    if (error.command === 'ERROR') {
-      console.error(error.headers.message);
-    } else {
-      console.log('client connected');
-      console.log(client);
-
-      client.subscribe(destination, (body, headers) => {
-        // return JSON.parse(body);
-      });
-    }
-  });
-};
